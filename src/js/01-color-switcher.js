2fab33209799ab509c8723d642a5af2063ef55a5
Vetik007 <boy0007@ukr.net>
2023-04-10 07:06:43 +0300

    add tascks 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+
+// console.log(startBtn);
+
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', colorStartBt);
+stopBtn.addEventListener('click', colorStopBtn);
+
+function randomColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function colorStartBt() {
+    randomColorId = setInterval(randomColor, 1000);
+    // console.log('hi');
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    // console.log('hi');
+};
+
+function colorStopBtn() {
+    clearInterval(randomColorId);
+    // console.log('bye');
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    // console.log('bye');
+};
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+
+    for (let i = 0; i < amount.value; i+=1) {
+      let position = i + 1;
+      const delays = Number(delay.value) + step.value * i;
+
+      createPromise(position, delays)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+
+  event.currentTarget.reset();
 }
